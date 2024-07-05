ecffb676a1fdc944179c4f6d169618a982df1d03
dualUbuntuLevi <leviqueiroz647@gmail.com>
2024-07-05 11:34:06 -0300

    Create Parameterized test for Cliente

--- trabalho1/src/test/java/com/trabalho1/testeCliente.java
+++ trabalho1/src/test/java/com/trabalho1/testeCliente.java
@@ -1,15 +1,48 @@
 package com.trabalho1;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.Test;
+import java.util.Arrays;
+import java.util.Collection;
 
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
 public class testeCliente {
 
+    String nome;
+    TipoCliente tipoClienteInicial;
+    Endereco endereco;
+    double comprasMensais;
+    TipoCliente tipoClienteEsperado;
+
+    @Parameters
+    public static Collection<Object[]> getParameters(){
+        Object[][] resposta = new Object[][] {
+            {"João", TipoCliente.PADRAO, new Endereco("DF", true), 50.0, TipoCliente.PADRAO},
+            {"André", TipoCliente.PADRAO, new Endereco("SP",false), 150.0, TipoCliente.ESPECIAL},
+            {"Maria", TipoCliente.ESPECIAL, new Endereco("MG",true), 200.0, TipoCliente.ESPECIAL},
+            {"Ana", TipoCliente.PRIME, new Endereco("RJ", true), 0.0, TipoCliente.PRIME}
+        };
+        return Arrays.asList(resposta);
+    }
+
+    public testeCliente(String nome, TipoCliente tipoClienteInicial, Endereco endereco, double comprasMensais, TipoCliente tipoClienteEsperado)
+    {
+        this.nome = nome;
+        this.tipoClienteInicial = tipoClienteInicial;
+        this.endereco = endereco;
+        this.comprasMensais = comprasMensais;
+        this.tipoClienteEsperado = tipoClienteEsperado;
+    }
+
     @Test
-    public void testeCadastroClientePadrao(){
-        Endereco endereco_1 = new Endereco("MG", true);
-        Cliente cliente_1 = new Cliente("André", TipoCliente.PADRAO, endereco_1);
-        assertEquals(TipoCliente.PADRAO, cliente_1.getTipoCliente());
+    public void testeCadastroCliente(){
+        Cliente cliente = new Cliente(nome, tipoClienteInicial, endereco);
+        cliente.setComprasMensais(comprasMensais);
+        assertEquals(tipoClienteEsperado, cliente.getTipoCliente());
     }
 
 }
