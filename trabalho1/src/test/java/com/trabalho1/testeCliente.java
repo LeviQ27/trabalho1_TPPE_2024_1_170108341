d0844ebd54bcfc4935fadef51d03011e07a6fd4c
dualUbuntuLevi <leviqueiroz647@gmail.com>
2024-07-06 18:34:04 -0300

    Refactor test to implement if statement on class

--- trabalho1/src/test/java/com/trabalho1/testeCliente.java
+++ trabalho1/src/test/java/com/trabalho1/testeCliente.java
@@ -16,25 +16,31 @@ public class testeCliente {
     TipoCliente tipoClienteInicial;
     Endereco endereco;
     double comprasMensais;
+    double mensalidade;
     TipoCliente tipoClienteEsperado;
 
     @Parameters
     public static Collection<Object[]> getParameters(){
         Object[][] resposta = new Object[][] {
-            {"João", TipoCliente.PADRAO, new Endereco("DF", true), 50.0, TipoCliente.PADRAO},
-            {"André", TipoCliente.PADRAO, new Endereco("SP",false), 150.0, TipoCliente.ESPECIAL},
-            {"Maria", TipoCliente.ESPECIAL, new Endereco("MG",true), 200.0, TipoCliente.ESPECIAL},
-            {"Ana", TipoCliente.PRIME, new Endereco("RJ", true), 0.0, TipoCliente.PRIME}
+            {"João", TipoCliente.PADRAO, new Endereco("DF", true), 50.0, 0.0, TipoCliente.PADRAO},
+            {"André", TipoCliente.PADRAO, new Endereco("SP",false), 150.0, 0.0, TipoCliente.ESPECIAL},
+            {"Maria", TipoCliente.ESPECIAL, new Endereco("MG",true), 200.0, 0.0, TipoCliente.ESPECIAL},
+            {"Ana", TipoCliente.PRIME, new Endereco("RJ", true), 0.0, 20.00, TipoCliente.PRIME},
+            {"Analice", TipoCliente.PADRAO, new Endereco("RJ", true), 0.0, 20.00, TipoCliente.PRIME},
+            {"Ana Clara", TipoCliente.ESPECIAL, new Endereco("RJ", true), 0.0, 20.00, TipoCliente.PRIME},
+            {"Júlia", TipoCliente.ESPECIAL, new Endereco("RJ", true), 0.0, 0.0, TipoCliente.PADRAO},
+            {"Vitória", TipoCliente.PRIME, new Endereco("RJ", true), 0.0, 0.0, TipoCliente.PADRAO},
         };
         return Arrays.asList(resposta);
     }
 
-    public testeCliente(String nome, TipoCliente tipoClienteInicial, Endereco endereco, double comprasMensais, TipoCliente tipoClienteEsperado)
+    public testeCliente(String nome, TipoCliente tipoClienteInicial, Endereco endereco, double comprasMensais, double mensalidade, TipoCliente tipoClienteEsperado)
     {
         this.nome = nome;
         this.tipoClienteInicial = tipoClienteInicial;
         this.endereco = endereco;
         this.comprasMensais = comprasMensais;
+        this.mensalidade = mensalidade;
         this.tipoClienteEsperado = tipoClienteEsperado;
     }
 
@@ -42,6 +48,7 @@ public class testeCliente {
     public void testeCadastroCliente(){
         Cliente cliente = new Cliente(nome, tipoClienteInicial, endereco);
         cliente.setComprasMensais(comprasMensais);
+        cliente.setMensalidade(mensalidade);
         assertEquals(tipoClienteEsperado, cliente.getTipoCliente());
     }
 
