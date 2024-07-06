e093d738d597f099708347de7aaa0271e85146ba
dualUbuntuLevi <leviqueiroz647@gmail.com>
2024-07-06 00:33:13 -0300

    Refactor Venda from testeVenda

--- trabalho1/src/main/java/com/trabalho1/Venda.java
+++ trabalho1/src/main/java/com/trabalho1/Venda.java
@@ -12,7 +12,6 @@ public class Venda {
     private double frete;
     private double icms;
     private double impostoMunicipal;
-    private String data;
 
     public Venda(Cliente cliente, List<Produto> produtos, String cartao){
         this.cliente = cliente;
@@ -80,39 +79,26 @@ public class Venda {
         this.impostoMunicipal = impostoMunicipal;
     }
 
-    public String getData() {
-        return data;
-    }
-
-    public void setData(String data) {
-        this.data = data;
-    }
-
     private double calcularValorTotal(String cartao) {
-        double valorTotal_1 = valorReal();
-        switch (this.cliente.getTipoCliente()) {
-            case ESPECIAL:
-                valorTotal_1 += calcularFrete() * 0.3;
-                break;
-            case PRIME:
-                break;
-            default:
-                valorTotal_1 += calcularFrete();
-                break;
-        }
-        return valorTotal_1 -= calcularDesconto(cartao);
+        double valorTotal_1 = valorReal() + calcularICMS() + calcularImpostoMunicipal();
+        valorTotal_1 += calcularFrete();
+        valorTotal_1 = Math.round(valorTotal_1*100.0)/100.0;
+
+        return ((int)((valorTotal_1 - calcularDesconto(cartao))*100))/100.0;
     }
 
     private double calcularDesconto(String cartao) {
         double desconto_1 = 0.0;
+        double valorReal_2 = valorReal() + calcularICMS() + calcularImpostoMunicipal();
+
         if (this.cliente.getTipoCliente() == TipoCliente.ESPECIAL) {
-            desconto_1 += valorReal() * 0.10;
+            desconto_1 += valorReal_2 * 0.10;
         }
 
 
-        if (this.cliente.getTipoCliente() == TipoCliente.ESPECIAL && verificarCartaoLoja(cartao))
+        if (verificarCartaoLoja(cartao))
         {
-            desconto_1 += valorReal() * 0.10;
+            desconto_1 += valorReal_2 * 0.10;
         }
         
 
@@ -122,70 +108,71 @@ public class Venda {
     private double calcularFrete() {
         double frete_1 = 0.0;
         
-        switch (this.cliente.getEndereco().getEstado()) {
-            case "AC":
-            case "AP":
-            case "AM":
-            case "PA":
-            case "RO":
-            case "RR":
-            case "TO":
-                if (this.cliente.getEndereco().iseCapital()) frete_1 = 20.00;
-                else frete_1 = 25.00;
-                break;
-            case "AL":
-            case "BA":
-            case "CE":
-            case "MA":
-            case "PB":
-            case "PE":
-            case "PI":
-            case "RN":
-            case "SE":
-                if (this.cliente.getEndereco().iseCapital()) frete_1 = 15.00;
-                else frete_1 = 18.00;
-                break;
-            case "GO":
-            case "MT":
-            case "MS":
-                if (this.cliente.getEndereco().iseCapital()) frete_1 = 10.00;
-                else frete_1 = 13.00;
-                break;
-            case "ES":
-            case "MG":
-            case "RJ":
-            case "SP":
-                if (this.cliente.getEndereco().iseCapital()) frete_1 = 7.00;
-                else frete_1 = 10.00;
-                break;
-            case "PR":
-            case "RS":
-            case "SC":
-                if (this.cliente.getEndereco().iseCapital()) frete_1 = 10.00;
-                else frete_1 = 13.00;
-                break;
-            default:
-                if (this.cliente.getEndereco().iseCapital()) frete_1 = 5.00;
-                break;
+        if (this.cliente.getTipoCliente() != TipoCliente.PRIME)
+        {
+            switch (this.cliente.getEndereco().getEstado()) {
+                case "AC":
+                case "AP":
+                case "AM":
+                case "PA":
+                case "RO":
+                case "RR":
+                case "TO":
+                    if (this.cliente.getEndereco().iseCapital()) frete_1 = 20.00;
+                    else frete_1 = 25.00;
+                    break;
+                case "AL":
+                case "BA":
+                case "CE":
+                case "MA":
+                case "PB":
+                case "PE":
+                case "PI":
+                case "RN":
+                case "SE":
+                    if (this.cliente.getEndereco().iseCapital()) frete_1 = 15.00;
+                    else frete_1 = 18.00;
+                    break;
+                case "GO":
+                case "MT":
+                case "MS":
+                    if (this.cliente.getEndereco().iseCapital()) frete_1 = 10.00;
+                    else frete_1 = 13.00;
+                    break;
+                case "ES":
+                case "MG":
+                case "RJ":
+                case "SP":
+                    if (this.cliente.getEndereco().iseCapital()) frete_1 = 7.00;
+                    else frete_1 = 10.00;
+                    break;
+                case "PR":
+                case "RS":
+                case "SC":
+                    if (this.cliente.getEndereco().iseCapital()) frete_1 = 10.00;
+                    else frete_1 = 13.00;
+                    break;
+                default:
+                    frete_1 += 5.00;
+                    break;
+            }
         }
         
-        return frete_1;
+        
+        return this.cliente.getTipoCliente() == TipoCliente.ESPECIAL ? frete_1 * 0.3 : frete_1;
     }
 
     private double calcularICMS() {
-        return this.cliente.getEndereco().getEstado().equals("DF") ? 0.18 : 0.12;
+        return this.cliente.getEndereco().getEstado().equals("DF") ? valorReal() * 0.18 : valorReal() * 0.12;
     }
 
     private double calcularImpostoMunicipal() {
-        return this.cliente.getEndereco().getEstado().equals("DF") ? 0 : 0.04;
+        return this.cliente.getEndereco().getEstado().equals("DF") ? 0 : valorReal() * 0.04;
     }
 
-    private  double valorReal()
+    private double valorReal()
     {
-        double valorReal = (produtos.stream().mapToDouble(Produto::getValorVenda).sum() * calcularImpostoMunicipal()) + (produtos.stream().mapToDouble(Produto::getValorVenda).sum() * calcularICMS()) + (produtos.stream().mapToDouble(Produto::getValorVenda).sum());
-
-        
-        return valorReal;
+        return produtos.stream().mapToDouble(Produto::getValorVenda).sum();
     }
 
     private boolean verificarCartaoLoja(String cartao){
